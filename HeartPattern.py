07ac3d80105c9e10b5709cfcf441968af3531c1d
Vishal580 <R20200530.VISHAL@cse.reva.edu.in>
2024-08-04 23:45:26 +0530

    Heart Pattern

--- HeartPattern.py
+++ HeartPattern.py
@@ -1,7 +1,8 @@
 for r in range(6):
     for c in range(7):
         if (r==0 and c%3!=0) or (r==1 and c%3==0) or (r-c == 2) or (r+c == 8):
-            print('❤',end=' ')
+            #print('❤',end=' ')
+            print("Anu💞", end=' ')
         else:
-            print(' ',end=' ')
+            print('   ',end='  ')
     print()
